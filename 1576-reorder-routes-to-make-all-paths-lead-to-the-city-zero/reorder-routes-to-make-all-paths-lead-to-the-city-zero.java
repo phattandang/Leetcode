0d47395e71ed298b6a59223f98ea13a9811b973c
Phat Dang <103609154+phattandang@users.noreply.github.com>
2025-06-14 22:45:16 -0400

    Time: 39 ms (76.02%) | Memory: 74 MB (48.05%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -12,7 +12,7 @@ class Solution {
     private int dfs(List<List<Integer>> al, boolean[] visited, int from) {
         int change = 0;
         visited[from] = true;
-        for(var to : al.get(from)) {
+        for(int to : al.get(from)) {
             if(!visited[Math.abs(to)]) {
                 change += dfs(al, visited, Math.abs(to)) + (to > 0 ? 1 : 0);
             }
